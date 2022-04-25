fd653440e57dd7d97b5fea73adcaa12f98c3ab2f
Moamen236 <moamen.ali107@gmail.com>
2022-04-25 04:26:40 +0200

    edit in payments

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -66,13 +66,13 @@ class PaymentsController extends Controller
                     'id' => $payment->id(),
                     'company_name' => $company->data()['name'],
                     'client_id' => $payment->data()['client_id'],
-                    'service_code' => (int) $payment->data()['service_code'],
-                    'price' => (double) $payment->data()['price'],
+                    'service_code' => $payment->data()['service_code'],
+                    'price' => (double) $payment->data()['price'] ?? 0,
                     'receipt' => [
                         'id' => $receipt->id(),
                         'payment_id' => $receipt->data()['payment_id'],
-                        'feeds' => (double) $receipt->data()['feeds'],
-                        'total' => (double) $receipt->data()['total'],
+                        'feeds' => (double) $receipt->data()['feeds'] ?? 0,
+                        'total' => (double) $receipt->data()['total'] ?? 0,
                         'date' => $receipt->data()['date']->get()->format('Y-m-d H:i:s'),
                     ]
                 ]
