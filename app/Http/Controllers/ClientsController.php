53a80cfc696b90d09664df283a369659228496b0
Moamen236 <moamen.ali107@gmail.com>
2022-04-25 03:19:01 +0200

    edit in payments

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -109,6 +109,7 @@ class ClientsController extends Controller
      */
     public function payments(Request $request)
     {
+        ini_set('max_execution_time', '300');
         $client = new client();
         $payments = $client->payments($request->id);
         if ($payments)
